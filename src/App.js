d3cc7131b9fd6e1007caa2293604183c9ba4bfb2
UWEGHREPHA EJIRO THANKGOD <ejineroz@pop-os.localdomain>
2022-04-14 10:09:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import "./App.css";
+
+const StyledRating = styled(Rating)({
+  "& .MuiRating-iconFilled": {
+    color: "#ff6d75",
+  },
+  "& .MuiRating-iconHover": {
+    color: "#ff3d47",
+  },
+});
+
+const labels = {
+  0.5: "Useless",
+  1: "Useless+",
+  1.5: "Poor",
+  2: "Poor+",
+  2.5: "Ok",
+  3: "Ok+",
+  3.5: "Good",
+  4: "Good+",
+  4.5: "Excellent",
+  5: "Excellent+",
+};
 
 function App() {
+  const [value, setValue] = React.useState(2);
+  const [hover, setHover] = React.useState(-1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>How would you rate our App?</p>
+        <Box
+          sx={{
+            width: 200,
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
+          <StyledRating
+            name="customized-color"
+            defaultValue={2}
+            getLabelText={(value) => `${value} Heart${value !== 1 ? "s" : ""}`}
+            precision={0.5}
+            icon={<FavoriteIcon fontSize="inherit" />}
+            emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+            size="large"
+            hover="hover-feedback"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            onChangeActive={(event, newHover) => {
+              setHover(newHover);
+            }}
+          />
+          {value !== null && (
+            <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+          )}
+        </Box>
+        <Stack spacing={5}>
+          <Box
+            sx={{
+              width: 500,
+              maxWidth: "100%",
+            }}
+          ></Box>
+          <TextField
+            fullWidth
+            label="feedback"
+            color="primary"
+            id="fullWidth"
+          />
+        </Stack>
+        <Stack spacing={5}>
+          <Box
+            sx={{
+              width: 500,
+              maxWidth: "100%",
+            }}
+          ></Box>
+          <Button variant="contained">Submit</Button>
+        </Stack>
       </header>
     </div>
   );
 }
-
 export default App;
